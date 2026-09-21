34859265a1379ad86415a5144626c2152adb66e4
Paul Marbach <paul.marbach@grafana.com>
2026-09-21 14:49:32 -0400

    feat(table): refresh cell actions menu

--- packages/grafana-ui/src/components/Table/TableNG/render-hooks.tsx
+++ packages/grafana-ui/src/components/Table/TableNG/render-hooks.tsx
@@ -353,7 +353,7 @@ function buildColumnsFromFields(
 
     // helps us avoid string cx and emotion per-cell
     const cellActionClassName = showActions
-      ? clsx('table-cell-actions', getCellActionStyles(theme, textAlign))
+      ? clsx('table-cell-actions', getCellActionStyles(theme, textAlign, tableRefreshEnabled))
       : undefined;
 
     const shouldOverflow =
@@ -472,6 +472,7 @@ function buildColumnsFromFields(
           />
           {showActions && (
             <TableCellActions
+              tableRefreshEnabled={tableRefreshEnabled}
               field={field}
               value={value}
               displayName={displayName}
